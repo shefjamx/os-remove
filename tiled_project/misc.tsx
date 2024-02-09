06f434117ee2916d9bb109bcbaac66c325e16820
David Gasinski <dgasinski1@sheffield.ac.uk>
2024-02-09 23:13:37 +0000

    draft level added

---
+++ tiled_project/misc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="misc" tilewidth="16" tileheight="16" tilecount="1105" columns="17">
+ <image source="../images/tileset/colony-db32-other-ready.png" width="272" height="1040"/>
+</tileset>
---
+++ tiled_project/objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="objects" tilewidth="16" tileheight="16" tilecount="2600" columns="20">
+ <image source="../images/tileset/colony-db32-buildings-ready.png" width="320" height="2080"/>
+</tileset>
