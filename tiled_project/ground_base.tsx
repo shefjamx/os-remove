e597045597a87ea5bdc9e8076e7b9037f3d69ced
David Gasinski <dgasinski1@sheffield.ac.uk>
2024-02-09 20:41:09 +0000

    basic art and tilesets

---
+++ tiled_project/ground_base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="ground_base" tilewidth="16" tileheight="16" tilecount="1020" columns="20">
+ <image source="../images/tileset/colony-db32-grounds-ready.png" width="320" height="816"/>
+</tileset>
