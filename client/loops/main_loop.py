6b40df45eb1f3c130ec5c9b1751bc75e8d3fed23
Julian <julianjones663@gmail.com>
2024-02-09 20:12:06 +0000

    setup game loop to use a generic scene

--- client/loops/main_loop.py
+++ client/loops/main_loop.py
@@ -1,12 +1,28 @@
 import pygame
 import sys
 
+from pygame.locals import (
+    K_w,  # Forwards
+    K_a,  # Left
+    K_s,  # Down
+    K_d,  # Right
+    K_k,  # Beat button 1
+    K_l,  # Beat button 2
+    KEYDOWN,
+    QUIT
+)
+
+from scenes.generic_scene import GenericScene
+from scenes.home import HomeScreen
+
 class MainLoop():
     """Main Loop class, this will handle all keyboard inputs and generic stuff (that i've not figured out yet)"""
 
     def __init__(self, screen) -> None:
         self.running = False
         self.screen = screen
+        self.clock = pygame.time.Clock()
+        self.current_scene: GenericScene = HomeScreen(screen)
 
     def start(self) -> None:
         """
@@ -16,12 +32,31 @@ class MainLoop():
         while self.running:
             # Event loop
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
+                if event.type == QUIT:
                     self.running = False
                     sys.exit()  # Stops the full python file - may fuck up stuff in future
 
-            # Draw some stuff
-            self.screen.fill((255, 255, 255))
-            
+                # Keydown
+                if event.type == KEYDOWN:
+                    if event.key == K_w:
+                        pass # Move Forwards
+                    elif event.key == K_a:
+                        pass # Move right
+                    elif event.key == K_s:
+                        pass # Move right
+                    elif event.key == K_d:
+                        pass # Move right
+                    elif event.key == K_k:
+                        pass # Move right
+                    elif event.key == K_l:
+                        pass # Move right
+
+            # Re-render the screen
+            if self.current_scene is not None:
+                self.current_scene.tick()
+
+            # Tick clock at 120fps
+            self.clock.tick(120)
+
         # Game is over
         pygame.quit()
---
+++ client/scenes/home.py
@@ -0,0 +1,8 @@
+from generic_scene import GenericScene
+
+class HomeScreen(GenericScene):
+    def __init__(self, screen) -> None:
+        super().__init__(screen)
+
+    def tick(self):
+        return super().tick()
