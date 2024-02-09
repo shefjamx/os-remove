97d51c3abfecdcc78dd9e0f104ee41bdea4631fa
Joe Paton <joantpat@gmail.com>
2024-02-09 22:27:00 +0000

    Updated level scene

--- client/scenes/level.py
+++ client/scenes/level.py
@@ -16,42 +16,46 @@ class LevelScene(GenericScene):
     def __init__(self, screen, pathToLevel: str):
         self.GRACE_PERIOD = 5 #countdown before the song plays in seconds
         super().__init__(screen)
-        self.__musicChannel = pygame.mixer.music
-        self.__levelIsRunning = False
-        self.__level = Level(pathToLevel)
-        self.__musicChannel.load(self.__level.getSongPath())
+        self.musicChannel = pygame.mixer.music
+        self.level = Level(pathToLevel)
+        self.musicChannel.load(self.level.getSongPath())
 
-        self.__status = self.Status.COUNTDOWN
-        self.__countdownStart = time.perf_counter()
+        self.status = self.Status.COUNTDOWN
+        self.countdownStart = time.perf_counter()
         self.font = pygame.font.Font("assets\\fonts\\Abaddon Bold.ttf", 96)
-        self.__graphics_attributes = {}
+        self.graphics_attributes = {}
     
-    def __render(self):
+    def render(self):
         self.screen.fill((0, 0, 0))
-        if self.__status == self.Status.COUNTDOWN:
+        if self.status == self.Status.COUNTDOWN:
             # Draw the countdown to the screen
-            timeTillStart = "{:.2f}".format(self.GRACE_PERIOD - (time.perf_counter() - self.__countdownStart))
+            timeTillStart = "{:.2f}".format(self.GRACE_PERIOD - (time.perf_counter() - self.countdownStart))
             _time = self.font.render(f"{timeTillStart}", False, "#FFFFFF")
-            if "time-width" not in self.__graphics_attributes:
-                self.__graphics_attributes["time-width"] = _time.get_width() + 10
+            if "time-width" not in self.graphics_attributes:
+                self.graphics_attributes["time-width"] = _time.get_width() + 10
             _text = self.font.render(f"s to Start", False, "#FFFFFF")
             self.screen.blit(_time,(0, 0))
-            self.screen.blit(_text, (self.__graphics_attributes["time-width"], 0))
+            self.screen.blit(_text, (self.graphics_attributes["time-width"], 0))
 
-    def __startLevel(self):
-        self.__status = self.Status.PLAYING
-        self.__musicChannel.play()
+    def startLevel(self):
+        self.status = self.Status.PLAYING
+        self.musicChannel.play()
     
-    def __doGameLogic(self):
-        if self.__status == self.Status.COUNTDOWN:
-            timeTillStart = self.GRACE_PERIOD - (time.perf_counter() - self.__countdownStart)
+    def doGameLogic(self):
+        if self.status == self.Status.COUNTDOWN:
+            timeTillStart = self.GRACE_PERIOD - (time.perf_counter() - self.countdownStart)
             if timeTillStart <= 0:
-                self.__startLevel()
-        elif self.__status == self.Status.PLAYING:
-            currentSongTime = self.__musicChannel.get_pos()
-
+                self.startLevel()
+        elif self.status == self.Status.PLAYING:
+            currentSongTime = self.musicChannel.get_pos()
+            # spawn any new enemies
+            # update enemy positions
+            # calculate enemy attacks
+            # check if center is still alive after attacks
+            # update player position
+            # check player attack based on currentSongTime    
     
     def tick(self):
-        self.__doGameLogic()
-        self.__render()
+        self.doGameLogic()
+        self.render()
         return super().tick()
