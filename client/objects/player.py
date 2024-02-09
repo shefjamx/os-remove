bb5e9920e8d1e9d09f995c604d5ffa7909ccd2a6
Julian <julianjones663@gmail.com>
2024-02-09 23:34:03 +0000

    what da playa

--- client/objects/player.py
+++ client/objects/player.py
@@ -3,3 +3,8 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self) -> None:
         super().__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    
--- client/scenes/play.py
+++ client/scenes/play.py
@@ -1,10 +1,13 @@
 import pygame
 from scenes.generic_scene import GenericScene
+from objects.player import Player
+from misc.settings import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class PlayScene(GenericScene):
     def __init__(self, screen, main_loop) -> None:
         super().__init__(screen, main_loop)
         self.background_image = pygame.image.load("assets/images/level_draft.png")
+        self.player = Player()
 
     def tick(self):
         # Background
@@ -12,5 +15,8 @@ class PlayScene(GenericScene):
         rect.x -= 640
         rect.y -= 360
         self.screen.blit(self.background_image, rect)
+
+        # Player
+        self.screen.blit(self.player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
 
         return super().tick()
