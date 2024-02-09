b732799950dd2d6a6b0b65feabb86f728140da31
Joe Paton <joantpat@gmail.com>
2024-02-09 22:39:43 +0000

    Updated input system for callbacks

--- client/loops/main_loop.py
+++ client/loops/main_loop.py
@@ -15,18 +15,40 @@ from pygame.locals import (
 from misc.pos import Pos
 from scenes.generic_scene import GenericScene
 from scenes.home import HomeScreen
+from scenes.level import LevelScene
 
 class MainLoop():
     """Main Loop class, this will handle all keyboard inputs and generic stuff (that i've not figured out yet)"""
 
     def __init__(self, screen) -> None:
         self.running = False
+        self.keyMap: dict = {}
         self.screen = screen
         self.clock = pygame.time.Clock()
-        self.current_scene: GenericScene = HomeScreen(screen, self)
+        #self.current_scene: GenericScene = HomeScreen(screen, self)
+        self.current_scene: GenericScene = LevelScene(screen, self, "ascension-to-heaven")
 
     def change_scene(self, scene: GenericScene):
         self.current_scene = scene(self.screen, self)
+
+    def addKeyCallback(self, key: int, callback) -> None:
+        """
+        Registers a callback to be called when the given keycode is registered as pressed
+        If there is already a callback registered then it is removed :D Dont care!!!
+
+        Parameters:
+            key[int]: The python.locals keycode used for the key
+            callback[callable]: The function to be called
+        TODO: Do we want there to be multiple callbacks for one keycode? Also add support for held keys!
+        """
+        self.keyMap[key] = callback
+
+    def removeKeyCallback(self, key: int) -> None:
+        """
+        Removes a created callback function
+        """
+        del self.keyMap[key]
+
 
     def start(self) -> None:
         """
@@ -47,18 +69,9 @@ class MainLoop():
 
                 # Keydown
                 if event.type == KEYDOWN:
-                    if event.key == K_w:
-                        pass # Move Forwards
-                    elif event.key == K_a:
-                        pass # Move right
-                    elif event.key == K_s:
-                        pass # Move right
-                    elif event.key == K_d:
-                        pass # Move right
-                    elif event.key == K_k:
-                        pass # Move right
-                    elif event.key == K_l:
-                        pass # Move right
+                    if event.key in self.keyMap:
+                        # call the button callback
+                        self.keyMap[event.key]()
 
             # Re-render the screen
             if self.current_scene is not None:
