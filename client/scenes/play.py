24178ad1512fb893bdf1a9ed440399d6b1454001
David Gasinski <dgasinski1@sheffield.ac.uk>
2024-02-10 01:52:18 +0000

    removed particle test

--- client/scenes/play.py
+++ client/scenes/play.py
@@ -10,16 +10,11 @@ class PlayScene(GenericScene):
         self.background_image = pygame.image.load("assets/images/level_draft.png")
         self.player = Player(main_loop)
 
-        #spawn in particles cause fuck it
-        self.flame_effect = FlameCirle(10, 5,[100, 100], 5)
-
     def tick(self):
         # Background
         rect = self.background_image.get_rect()
         rect.x = -self.player.x
         rect.y = -self.player.y
         self.display.blit(self.background_image, rect)
-
-        self.flame_effect.tick(self.display, self.main_loop.dt)
 
         return super().tick(self.player)
