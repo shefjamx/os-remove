d539dafd8b6328ef8f1f7a0c50569dfe766ee5d9
Julian <julianjones663@gmail.com>
2024-02-10 12:35:07 +0000

    necromancer

--- client/objects/necromancer.py
+++ client/objects/necromancer.py
@@ -1,12 +1,23 @@
 import pygame
 from objects.enemy import GenericEnemy
+from misc.animator import Tileset
 
 class Necromancer(GenericEnemy):
-    def __init__(self, x: float, y: float, sprite: str, damage: int, health: int) -> None:
-        super().__init__(x, y, sprite, damage, health)
+    def __init__(self, x, y, entity, main_loop) -> None:
+        super().__init__(x, y, 100, 1, main_loop)
+        self.dseireableEntity = entity
+        self.main_loop = main_loop
+        self.tilesets = {
+            "idle": Tileset("assets/images/necromancer/idle.png", (128, 128), 0, 7)
+        }
+        self.tileset = "idle"
+
+        # Sprite
+        self.surf = self.tilesets[self.tileset].increment()
+        self.player_react = self.surf.get_rect()
 
     def update(self):
         pass
 
-    def draw(self, surface: pygame.Surface) -> None:
-        surface.blit(self.sprite, (self.pos[0] - self.player_pos[1], self.pos[1] - self.player_pos[1]))
+    def draw(self, surface: pygame.Surface, playerPos) -> None:
+        surface.blit(self.surf, (self.pos[0] - self.player_pos[1], self.pos[1] - self.player_pos[1]))
