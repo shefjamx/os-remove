4ee39214b52e9908b854ee3ed6190651f47cae32
Julian <julianjones663@gmail.com>
2024-02-10 03:18:11 +0000

    added ctypes

--- client/main.py
+++ client/main.py
@@ -1,10 +1,12 @@
 import pygame
+import ctypes
 
 from misc.logger import log
 from misc.settings import SCREEN_WIDTH, SCREEN_HEIGHT
 from loops.main_loop import MainLoop
 
 # Initialize pygame
+ctypes.windll.user32.SetProcessDPIAware()
 pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 pygame.display.set_caption("OS, Remove?")
