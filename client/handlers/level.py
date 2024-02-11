1f4e3d0325fae1eb6a720b1ce417f7d3673233e9
Joe Paton <joantpat@gmail.com>
2024-02-11 11:36:45 +0000

    Updated level poggies

--- client/handlers/level.py
+++ client/handlers/level.py
@@ -10,6 +10,8 @@ class LevelHandler:
         self.mainLoop.add_key_callback(pygame.locals.K_RIGHT, lambda: self.incrementSelection(1), False)
         self.mainLoop.add_key_callback(pygame.locals.K_LEFT, lambda: self.incrementSelection(-1), False)
         self.mainLoop.add_key_callback(pygame.locals.K_RETURN, lambda: self.dispatchCallback(), False)
+        self.maxLevels = 3
+        self.currentPage = 0
         self.label_font = pygame.font.Font("assets/fonts/Abaddon Bold.ttf", 48)
 
         self.currentLevel = 0
@@ -17,6 +19,7 @@ class LevelHandler:
     def incrementSelection(self, amt) -> None:
         self.currentLevel += amt
         self.currentLevel = self.currentLevel % len(self.allLevels)
+        self.currentPage = self.currentLevel // self.maxLevels
         self.mainLoop.client.update_song(self.allLevels[self.currentLevel].directory)
         pygame.mixer.music.load(self.allLevels[self.currentLevel].getSongPath())
         pygame.mixer.music.play()
@@ -32,9 +35,9 @@ class LevelHandler:
         return pygame.Rect((self.mainLoop.screen.get_width() - LEVEL_WIDTH ) / 2, 200 + (LEVEL_HEIGHT + LEVEL_MARGIN)*i, LEVEL_WIDTH, LEVEL_HEIGHT)
 
     def draw(self, surface):
-        for i,level in enumerate(self.allLevels):
+        for i,level in enumerate(self.allLevels[self.currentPage*3:min((self.currentPage+1)*3, len(self.allLevels))]):
             rect = self.createLevelRect(level, i)
-            color = "#FF0000" if not self.currentLevel == i else "#00FF00"
+            color = "#FF0000" if not (self.currentLevel + self.currentPage*3) == i else "#00FF00"
             pygame.draw.rect(surface, color, rect)
             title = self.label_font.render(level.name, False, "#FFFFFF")
             surface.blit(title, (rect.x + (rect.w - title.get_width()) / 2, rect.y + 10))
