a8a361a07145ae7424704031b403a7525390e4f3
Joe Paton <joantpat@gmail.com>
2024-02-11 11:37:48 +0000

    skibidi miss

--- client/loops/main_loop.py
+++ client/loops/main_loop.py
@@ -40,7 +40,7 @@ class MainLoop():
         #self.current_scene: GenericScene = PlayScene(screen, self, "ascension-to-heaven", time.time() + 5, debug=True)
         # self.current_scene: GenericScene = PlayScene(screen, self, "cover-femboy-friday", time.time() + 5, debug=True)
         # self.current_scene: GenericScene = PlayScene(screen, self, "loser-baby", time.time() + 5, debug=True)
-        self.current_scene: GenericScene = LevelEditor(screen, self, "loser-baby")
+        #self.current_scene: GenericScene = LevelEditor(screen, self, "loser-baby")
         self.dt = 0
 
         log("Creating client")
